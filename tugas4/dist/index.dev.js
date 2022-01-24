5d070b58806c67c4bfb4d17549113bb66f6c0bfb
DanangFirmantara <danangfirmantara837@gmail.com>
2022-01-24 11:11:52 +0700

    testing

--- tugas4/dist/index.dev.js
+++ tugas4/dist/index.dev.js
@@ -3,45 +3,24 @@
 var fetch = require('cross-fetch');
 
 url = 'https://jsonplaceholder.typicode.com/users'; //menggunakan asyn await
-
-function listName() {
-  var data, result;
-  return regeneratorRuntime.async(function listName$(_context) {
-    while (1) {
-      switch (_context.prev = _context.next) {
-        case 0:
-          _context.next = 2;
-          return regeneratorRuntime.awrap(fetch(url));
-
-        case 2:
-          data = _context.sent;
-          _context.next = 5;
-          return regeneratorRuntime.awrap(data.json());
-
-        case 5:
-          result = _context.sent;
-          result.forEach(function (item) {
-            console.log(item.name);
-          });
-
-        case 7:
-        case "end":
-          return _context.stop();
-      }
-    }
-  });
-} //menggunakan try and catch
-// result = fetch(url).then(res =>{
-//     res.json().then(fin =>{
-//         fin.forEach(Element =>{
-//             console.log(Element.name)
-//         })
-//     }).catch(res =>{
-//         console.log("catch 1")
+// async function listName(){
+//     const data = await fetch(url)
+//     const result = await data.json()
+//     result.forEach(item =>{
+//         console.log(item.name)
 //     })
-// }).catch(err =>{
-//     console.log("catch 2")
-// })
+// }
+//menggunakan try and catch
 
-
-listName();
+result = fetch(url).then(function (res) {
+  res.json().then(function (fin) {
+    fin.forEach(function (Element) {
+      console.log(Element.name);
+    });
+  })["catch"](function (res) {
+    console.log("catch 1");
+  });
+})["catch"](function (err) {
+  console.log("catch 2");
+});
+console.log(result); // listName()
--- tugas4/index.js
+++ tugas4/index.js
@@ -4,27 +4,27 @@ url = 'https://jsonplaceholder.typicode.com/users'
 
 
 //menggunakan asyn await
-async function listName(){
-    const data = await fetch(url)
-    const result = await data.json()
-    result.forEach(item =>{
-        console.log(item.name)
-    })
-}
+// async function listName(){
+//     const data = await fetch(url)
+//     const result = await data.json()
+//     result.forEach(item =>{
+//         console.log(item.name)
+//     })
+// }
 
 
 //menggunakan try and catch
-// result = fetch(url).then(res =>{
-//     res.json().then(fin =>{
-//         fin.forEach(Element =>{
-//             console.log(Element.name)
-//         })
-//     }).catch(res =>{
-//         console.log("catch 1")
-//     })
-// }).catch(err =>{
-//     console.log("catch 2")
-// })
+result = fetch(url).then(res =>{
+    res.json().then(fin =>{
+        fin.forEach(Element =>{
+            console.log(Element.name)
+        })
+    }).catch(res =>{
+        console.log("catch 1")
+    })
+}).catch(err =>{
+    console.log("catch 2")
+})
 
-
-listName()
+console.log(result)
+// listName()
